cf42488f04588a47be10d44a5f3302a370b33ea2
William <william.delgiudice@kearsarge.org>
2020-01-14 22:04:21 -0500

    d

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
 
-int main(){
-    std::cout << "hello world" << std::endl;
+class Person
+{
+public:
+    short weight, age;
+    std::string name;
+    Person(std::string _name, short _age, short _weight)
+    {
+        name = _name;
+        age = _age;
+        weight = _weight;
+    }
+};
+
+int main()
+{
+    Person g("will", 17, 145);
+    std::cout << g.name << " is " << std::to_string(g.age) << " years old and weighs " << g.weight << " pounds" << std::endl;
+    std::cin;
     return 0;
 }
