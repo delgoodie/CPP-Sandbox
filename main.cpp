dab97e80ab922b41599f6b04603685537a2c1d86
William <william.delgiudice@kearsarge.org>
2020-01-16 14:40:59 -0500

    updated with terr enum

--- main.cpp
+++ main.cpp
@@ -2,42 +2,109 @@
 #include <cstdio>
 #include <math.h>
 
-const int MAP_HEIGHT = 5;
-const int MAP_WIDTH = 5;
-
-void setup()
+const int MAP_HEIGHT = 10;
+const int MAP_WIDTH = 20;
+const int MAP_TERRITORY_SIZE = 40;
+enum Territory
 {
-}
-
-class Territory
-{
-public:
-    int coords[2];
-    enum Status = {NULL = 0, HEALTHY, INFECTED, DEAD}; // all map coords are territories but can be null spaces or land units
-    Status status = Status.NULL;
-    Territory();
-
-private:
+    EMPTY,
+    HEALTHY,
+    INFECTED,
+    DEAD
 };
 
-Territory::Territory()
+int randInt(int max)
 {
+    return (int)(((double)rand() / (double)RAND_MAX) * max);
 }
 
-void printMapconst()
+void printMap(Territory *map[MAP_HEIGHT][MAP_WIDTH])
 {
-    //re-written for territory array
+    std::cout << "MAP:" << std::endl;
+    for (int i = 0; i < MAP_HEIGHT; i++)
+    {
+        for (int j = 0; j < MAP_WIDTH; j++)
+        {
+            switch (*map[i][j])
+            {
+            case EMPTY:
+                std::cout << "  ";
+                break;
+            case HEALTHY:
+                std::cout << "O ";
+                break;
+            case INFECTED:
+                std::cout << "% ";
+                break;
+            case DEAD:
+                std::cout << "X ";
+            }
+        }
+        std::cout << std::endl;
+    }
 }
 
-void loop()
+void setup(Territory *map[MAP_HEIGHT][MAP_WIDTH])
 {
+    int startX = randInt(MAP_WIDTH);
+    int startY = randInt(MAP_HEIGHT);
+    int numMoves = randInt(MAP_TERRITORY_SIZE);
+    for (int i = 0; i < MAP_HEIGHT; i++)
+    {
+        for (int j = 0; j < MAP_WIDTH; j++)
+        {
+
+            *map[i][j] = EMPTY;
+        }
+    }
+
+    *map[startY][startX] = HEALTHY;
+    for (int i = 0; i < numMoves; i++)
+    {
+        bool done = false;
+        bool end = false;
+        int x, y;
+        int count = 0;
+        while (!done)
+        {
+            y = startY + (rand() % 3 - 1);
+            x = startX + (rand() % 3 - 1);
+            count++;
+            if (x >= 0 && x < MAP_WIDTH && y >= 0 && y < MAP_HEIGHT && *map[y][x] == EMPTY)
+            {
+                *map[y][x] = HEALTHY;
+                startX = x;
+                startY = y;
+                done = true;
+            }
+            else if (count > 20)
+            {
+                end = true;
+                done = true;
+            }
+        }
+        if (end)
+            break;
+    }
+
+    printMap(map);
+}
+
+bool loop()
+{
+    return true;
 }
 
 int main()
 {
+    bool gameOver = false;
+    Territory *map[MAP_HEIGHT][MAP_WIDTH];
+
+    setup(map);
     while (!gameOver)
     {
-        loop();
+        if (loop())
+            gameOver = true;
     }
     std::cout << "Game Over!" << std::endl;
     std::cin;
