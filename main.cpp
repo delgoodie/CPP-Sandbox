46678f7c701d1ec6c4324c12f33fd5d614e2c079
William Del Giudice <delwillgiudice@gmail.com>
2021-03-18 12:26:25 -0400

    listnode

--- main.cpp
+++ main.cpp
@@ -4,8 +4,31 @@
 #include <string>
 #include "infection.h"
 
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode()
+    {
+        val = 0;
+        next = nullptr;
+    }
+    ListNode(int v)
+    {
+        val = v;
+        next = nullptr;
+    }
+    ListNode(int v, ListNode *n)
+    {
+        val = v;
+        next = n;
+    }
+};
+
 int main()
 {
-    infectionGame();
+    ListNode g = ListNode();
+    std::cout << g.val << std::endl;
+    std::cin.ignore();
     return 0;
 }
